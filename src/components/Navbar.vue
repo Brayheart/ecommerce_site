4e913cbe8046b356107cd3c163364c52f23832c2
Tyler Bray <broncobray77@gmail.com>
2018-05-29 11:09:10 -0700

    added Login and Singup Components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="navbar">
+        <nav class="green darken-1">
+            <div class="container">
+                <router-link :to="{name: 'GMap'}">Lorem!</router-link>
+                <ul class="right">
+                    <li><router-link :to="{ name: 'Signup'}">Signup</router-link></li>
+                    <li><router-link :to="{ name: 'Login'}">Login</router-link></li>
+                    <li><a @click="logout">Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '@/components/Welcome'
+import Signup from '../components/Auth/Signup.vue'
+import Login from '../components/Auth/Login.vue'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Welcome',
       component: Welcome
+    },
+    {
+      path: '/',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
